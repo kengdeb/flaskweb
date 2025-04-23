90dee86c5366ab669a4e4abfe1babda93c16372f
Keng <kengdeb@gmail.com>
2025-04-23 12:55:44 +0700

    modified 2

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def download_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     app.run(debug=True)
