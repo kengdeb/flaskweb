f3c57945ec5842d3e5ebc8223fc196193d0be15c
Keng <kengdeb@gmail.com>
2025-04-30 10:43:59 +0700

    remove debug equal True

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def download_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
 # if __name__ == '__main__':
 #     app.run(debug=True)
